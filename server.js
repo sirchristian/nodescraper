2ad6e1275d2ffec46ad934a6362188c58940a79e
Christian Heinzmann <github@sirchristian.net>
2015-05-10 15:56:03 -0400

    Some basics

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-var http = require("http");
+var express = require('express');
+var bodyParser = require('body-parser');
+var scraper = require('./lib/scraper');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/static/usage.html');
+});
+
+app.post('/scrape', function (request, response) {
+  var d = require('domain').create();
+  d.on('error', function(err){
+    response.send('Error Parsing JSON');
+  });
+
+  d.run(function() {
+    scraper.scrape(request.body.args.url, function(title){
+      console.log('title: ' + title);
+      response.send(title);
+    })
+  });
+});
+
+app.use(express.static('static'));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Listening at http://%s:%s', host, port);
+
+});
